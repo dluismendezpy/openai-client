82864faf20904354af636a2f97373a243a1282d9
Luis Mendez <dluismendezdj@gmail.com>
2023-01-21 13:51:48 -0400

    fix: include vite config into tsconfig

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
 import path from "path";
+
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 import eslint from "vite-plugin-eslint";
 
 // https://vitejs.dev/config/
